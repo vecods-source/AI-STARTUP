418d407da62942fb8c7a55b792f33f1314b65064
Mohamed Saad <saaddevlife@gmail.com>
2025-03-04 01:44:56 +0300

    added animation on first two section, testemonials features and product animation remaining tommorow morning first thing

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,24 +1,63 @@
+"use client";
 import Button from "@/components/Button";
 import starBg from "@/assets/stars.png";
+import { useRef } from "react";
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 export const Hero = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  useMotionValueEvent(scrollYProgress, "change", (value) => {
+    console.log(value);
+  });
+  const backgroundPositionY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [-300, 300]
+  );
   return (
-    <section
+    <motion.section
+      ref={sectionRef}
       className="flex h-[492px] md:h-[800px] items-center relative overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
-      style={{ backgroundImage: `url(${starBg.src})` }}
+      style={{ backgroundImage: `url(${starBg.src})`, backgroundPositionY }}
+      animate={{ backgroundPositionX: starBg.width }}
+      transition={{ duration: 120, repeat: Infinity, ease: "linear" }}
     >
       <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,.5)_15%,rgb(14,0,36,.5)_78%,transparent)]"></div>
-      <div className="w-[344px] h-[344px] md:w-[580px] md:h-[580px] border border-white opacity-10 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full">
+      <motion.div
+        animate={{ rotate: "1turn" }}
+        transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+        style={{ translateX: "-50%", translateY: "-50%" }}
+        className="w-[344px] h-[344px] md:w-[580px] md:h-[580px] border border-white opacity-10 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full"
+      >
         <div className="w-2 h-2 rounded-full bg-white absolute top-1/2 -left-0 -translate-x-1/2 -translate-y-1/2"></div>
         <div className="w-4 h-4 rounded-full border border-white absolute top-1/2 left-[100%] -translate-x-1/2 -translate-y-1/2">
           <div className="w-2 h-2 rounded-full bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
         </div>
         <div className="w-2 h-2 rounded-full bg-white absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
-      </div>
-      <div className="w-[444px] h-[444px] md:h-[780px] md:w-[780px] border border-dashed border-white opacity-15 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full"></div>
-      <div className="w-[544px] h-[544px] md:h-[980px] md:w-[980px] border border-white opacity-15 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full">
+      </motion.div>
+      <motion.div
+        animate={{ rotate: "-1turn" }}
+        transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+        style={{ translateX: "-50%", translateY: "-50%" }}
+        className="w-[444px] h-[444px] md:h-[780px] md:w-[780px] border border-dashed border-white opacity-15 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full"
+      ></motion.div>
+      <motion.div
+        animate={{ rotate: "1turn" }}
+        transition={{ duration: 90, repeat: Infinity, ease: "linear" }}
+        style={{ translateX: "-50%", translateY: "-50%" }}
+        className="w-[544px] h-[544px] md:h-[980px] md:w-[980px] border border-white opacity-15 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 rounded-full"
+      >
         <div className="w-2 h-2 rounded-full bg-white absolute top-1/2 -left-0 -translate-x-1/2 -translate-y-1/2"></div>
         <div className="w-2 h-2 rounded-full bg-white absolute top-1/2 left-[100%] -translate-x-1/2 -translate-y-1/2"></div>
-      </div>
+      </motion.div>
       <div className="w-64 h-64 md:w-96 md:h-96 rounded-full absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] border border-white/45 bg-purple-500 shadow-[-20px_-20px_50px_rgb(255,255,255,0.5),-20px_-20px_80px_rgb(255,255,255,0.1),0_0_50px_rgb(140,69,255)]"></div>
       <div className="container flex items-center flex-col gap-5 relative">
         <h1 className="text-center text-8xl md:text-[168px] md:leading-none text-transparent bg-clip-text font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))]">
@@ -30,6 +69,6 @@ export const Hero = () => {
         </p>
         <Button>Join waitlist</Button>
       </div>
-    </section>
+    </motion.section>
   );
 };
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import logo from "@/assets/logo-acme.png";
 import logo2 from "@/assets/logo-apex.png";
 import logo3 from "@/assets/logo-celestial.png";
@@ -5,6 +6,7 @@ import logo4 from "@/assets/logo-echo.png";
 import logo5 from "@/assets/logo-pulse.png";
 import logo6 from "@/assets/logo-quantum.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 export const LogoTicker = () => {
   return (
     <section>
@@ -13,20 +15,35 @@ export const LogoTicker = () => {
           <div className="flex flex-1 md:flex-none">
             <h2 className="text-white">Trusted by top innovative teams</h2>
           </div>
-          <div className="flex overflow-hidden flex-1 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex w-[157px] gap-14">
-              {[logo, logo2, logo3, logo4, logo5, logo6].map(
-                (logoSrc, index) => (
-                  <Image
-                    className="h-6"
-                    key={index}
-                    src={logoSrc}
-                    alt={`Logo ${index + 1}`}
-                  />
-                )
-              )}
-            </div>
-          </div>
+          <motion.div className="flex overflow-hidden flex-1 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              animate={{ x: "-50%" }}
+              transition={{ duration: 30, repeat: Infinity, ease: "linear" }}
+              className="flex flex-none  gap-14 pr-14 "
+            >
+              {[
+                logo,
+                logo2,
+                logo3,
+                logo4,
+                logo5,
+                logo6,
+                logo,
+                logo2,
+                logo3,
+                logo4,
+                logo5,
+                logo6,
+              ].map((logoSrc, index) => (
+                <Image
+                  className="h-6 w-auto"
+                  key={index}
+                  src={logoSrc}
+                  alt={`Logo ${index + 1}`}
+                />
+              ))}
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </section>
